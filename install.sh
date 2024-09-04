ac545cd0d9adb14d02ac6692785080b2356b5356
Вася Панков <frostkslo1@yandex.ru>
2024-09-04 18:24:11 +0300

    feat: добавлена аналитическая геометрия и алгебра

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-TODO
+#!/bin/bash
+
+while IFS= read -r line
+do
+    rm -rf temp
+    IFS=, read -r url name <<< "$line"
+    git clone "$url" temp
+    mkdir -p "$name"
+    cp -r temp/* "$name/"
+    rm -rf temp
+done < "repos.info" 
+
